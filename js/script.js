72085df8eb6865e479be12afda65bd3f83ec318a
Marco Peters <m.peters646@gmail.com>
2021-03-19 16:35:31 +0100

    Updated script & index files

--- js/script.js
+++ js/script.js
@@ -12,9 +12,44 @@ const addMoviesToDom = () => {
 addMoviesToDom();
 
 /* Radio buttons filter by select */
-const selectedRadioButton = document.querySelector('input[name="film-filter"]').value;
-const selectedMovies = selectedRadioButton.addEventListeners('change', () => {
-  console.log(selectedRadioButton);
-});
 
-console.log(selectedMovies);
+const movieTitles = movies.map((movie) => movie.Title);
+console.log(movieTitles);
+const filterMovies = (wordInMovieTitle) => {
+  let selectedMovies = movieTitles.find((movie) => movie.Title == wordInMovieTitle);
+  console.log(`wordInMovieTitle: ${wordInMovieTitle} | type: ${typeof wordInMovieTitle}`);
+  console.log(`selectedMovies: ${selectedMovies} | type: ${typeof selectedMovies}`);
+  console.log(`movieTitles: ${movieTitles} | type: ${typeof movieTitles}`);
+};
+
+const handleOnChangeEvent = document.body.addEventListener('change', (event) => {
+  let target = event.target;
+  let message;
+  switch (target.value) {
+    case 'alle-films':
+      message = 'The Alle films radio button changed';
+      break;
+    case 'nieuwste-films':
+      message = 'The Nieuwste Films radio button changed';
+      break;
+    case 'avengers-films':
+      // message = 'The Avengers Films radio button changed';
+      filterMovies('Avengers');
+      break;
+    case 'x-men-films':
+      // message = 'The X-Men Films radio button changed';
+      filterMovies('X-men');
+      break;
+    case 'princess-films':
+      filterMovies('Princess');
+      // message = 'The Princess Film radio button changed';
+      break;
+    case 'batman-films':
+      // message = 'The Batman Films radio button changed';
+      filterMovies('Batman');
+      break;
+  }
+  // console.log(filterMovies);
+  // console.log(message);
+  // return message;
+});
