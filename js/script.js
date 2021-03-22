42f6e3560d25f96f02489ba08b38b888254ddda8
Marco Peters <m.peters646@gmail.com>
2021-03-22 15:51:00 +0100

    Updated script.js - consoled array filtered movies

--- js/script.js
+++ js/script.js
@@ -9,23 +9,19 @@ const addMoviesToDom = () => {
     .join('');
 };
 
-addMoviesToDom();
-
 /* Radio buttons filter by select */
-// let movieTitles = movies.filter((movie) => movie.Title);
-
 const filterMovies = (wordInMovieTitle) => {
   const movieTitles = [];
   movies.forEach((movie) => {
     return movieTitles.push(movie.Title);
   });
 
-  movieTitles.filter((movieTitle) => {
-    console.log(movieTitle.includes(wordInMovieTitle));
+  let filter = movieTitles.filter((movieTitle) => {
+    return movieTitle.includes(wordInMovieTitle);
   });
+  console.log(filter);
+  return filter;
 };
-
-console.log(movies);
 
 const handleOnChangeEvent = document.body.addEventListener('change', (event) => {
   let target = event.target;
@@ -38,22 +34,19 @@ const handleOnChangeEvent = document.body.addEventListener('change', (event) => 
       message = 'The Nieuwste Films radio button changed';
       break;
     case 'avengers-films':
-      // message = 'The Avengers Films radio button changed';
       filterMovies('Avengers');
       break;
     case 'x-men-films':
-      // message = 'The X-Men Films radio button changed';
-      filterMovies('X-men');
+      filterMovies('X-Men');
       break;
     case 'princess-films':
       filterMovies('Princess');
-      // message = 'The Princess Film radio button changed';
       break;
     case 'batman-films':
-      // message = 'The Batman Films radio button changed';
       filterMovies('Batman');
       break;
   }
   // console.log(message);
-  // return message;
 });
+
+addMoviesToDom();
