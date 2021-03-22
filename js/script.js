88cd6d8ae5a9ab07dccf19f697c819c022c513c7
Marco Peters <m.peters646@gmail.com>
2021-03-22 19:27:28 +0100

    Finished the filter buttons in script.js

--- js/script.js
+++ js/script.js
@@ -1,52 +1,55 @@
 /* Shows the filmposters to the DOM */
 const movieList = document.getElementById('movieList');
 
-const addMoviesToDom = () => {
-  movieList.innerHTML = movies
-    .map((movie) => {
-      return `<li><a href="https://www.imdb.com/title/${movie.imdbID}" target="_blank"><img src="${movie.Poster}"></a></li>`;
-    })
-    .join('');
-};
+const addMoviesToDom = (array) =>
+  array.forEach((movie) => {
+    const newLi = document.createElement('li');
+    const newLink = document.createElement('a');
+    const poster = document.createElement('img');
+
+    newLink.target = '_blank';
+    newLink.href = 'https://www.imdb.com/title/' + movie.imdbID;
+    poster.src = movie.Poster;
+
+    newLi.appendChild(newLink).appendChild(poster);
+    movieList.appendChild(newLi);
+  });
+
+addMoviesToDom(movies);
 
 /* Radio buttons filter by select */
-const filterMovies = (wordInMovieTitle) => {
-  const movieTitles = [];
-  movies.forEach((movie) => {
-    return movieTitles.push(movie.Title);
-  });
+const newMovies = movies.filter((movie) => movie.Year >= 2014);
+const filteredMovies = (wordInMovieTitle) =>
+  movies.filter((movie) => movie.Title.toLocaleLowerCase().includes(wordInMovieTitle.toLocaleLowerCase()));
 
-  let filter = movieTitles.filter((movieTitle) => {
-    return movieTitle.includes(wordInMovieTitle);
-  });
-  console.log(filter);
-  return filter;
-};
-
+/* Radio buttons change */
 const handleOnChangeEvent = document.body.addEventListener('change', (event) => {
   let target = event.target;
   let message;
   switch (target.value) {
     case 'alle-films':
-      message = 'The Alle films radio button changed';
+      movieList.innerHTML = '';
+      addMoviesToDom(movies);
       break;
     case 'nieuwste-films':
-      message = 'The Nieuwste Films radio button changed';
+      movieList.innerHTML = '';
+      addMoviesToDom(newMovies);
       break;
     case 'avengers-films':
-      filterMovies('Avengers');
+      movieList.innerHTML = '';
+      addMoviesToDom(filteredMovies('Avengers'));
       break;
     case 'x-men-films':
-      filterMovies('X-Men');
+      movieList.innerHTML = '';
+      addMoviesToDom(filteredMovies('x-men'));
       break;
     case 'princess-films':
-      filterMovies('Princess');
+      movieList.innerHTML = '';
+      addMoviesToDom(filteredMovies('Princess'));
       break;
     case 'batman-films':
-      filterMovies('Batman');
+      movieList.innerHTML = '';
+      addMoviesToDom(filteredMovies('batman'));
       break;
   }
-  // console.log(message);
 });
-
-addMoviesToDom();
